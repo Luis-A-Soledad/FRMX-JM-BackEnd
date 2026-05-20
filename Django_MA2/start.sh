b178c485c1311bcc2d27e019c04a51265f2d1e57
Angelica Perez <Angelica.Perez1+gmxt@mx.ey.com>
2026-05-20 14:36:37 -0600

    Actualizacion de archivos de arranque

---
+++ Django_MA2/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Activar entorno virtual si usas uno, por ejemplo:
+source antenv/bin/activate
+echo "Ejecutando migraciones..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "Recolectando archivos estáticos..."
+python3 manage.py collectstatic --noinput
+echo "Iniciando Gunicorn..."
+python3 -m gunicorn core.wsgi:application -c gunicorn.conf.py
