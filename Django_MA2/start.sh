ffbf8023980e67dd095daa44bfb686d1af9f4860
Angelica Perez <Angelica.Perez1+gmxt@mx.ey.com>
2026-05-21 13:34:50 -0600

    modifciación de sh

--- Django_MA2/start.sh
+++ Django_MA2/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Activar entorno virtual si usas uno, por ejemplo:
 source antenv/bin/activate
-echo "Ejecutando migraciones..."
-python3 manage.py makemigrations
-python3 manage.py migrate
+#echo "Ejecutando migraciones..."
+#python3 manage.py makemigrations
+#python3 manage.py migrate
 echo "Recolectando archivos estaticos..."
 python3 manage.py collectstatic --noinput
 echo "Iniciando Daphne..."
