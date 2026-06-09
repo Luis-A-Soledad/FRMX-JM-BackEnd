1831fb4a1d177eca8793aa326de9dd26cbf78189
David Carmona <David.Carmona@mx.ey.com>
2026-06-08 20:46:37 -0600

    feat: Cambio temporal en DistritosView: se agregaron las coordenadas de algunos distritos

---
+++ Django_MA2/api/distritos.py
@@ -0,0 +1,7 @@
+DISTRITOS = {
+    "Parral": {"lat": 26.9333, "lng": -105.6667},
+    "La Barca": {"lat": 20.2833, "lng": -102.5500},
+    "Bachimba": {"lat": 28.8, "lng": -105.9},
+    "Viborillas": {"lat": 22.5, "lng": -102.2},
+    "Colima": {"lat": 19.2452, "lng": -103.7241},
+}
